65dd1320ae8e2dc5a5f2bf2c1216ba4849cd50bf
Holman Alba <holman.alba@repremundo.com.co>
2025-10-26 22:37:52 -0500

    fix(entrypoint): generate nginx config dynamically and run artisan setup at runtime
    fix(entrypoint): generate nginx config dynamically and run artisan setup at runtime

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
+envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 php artisan storage:link || true
 php artisan config:cache || true
 php artisan route:cache || true
